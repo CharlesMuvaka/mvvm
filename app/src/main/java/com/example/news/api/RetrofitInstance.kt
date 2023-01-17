3a65483ac5498f67ab5102e99fb042218b6ba8f8
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-17 08:17:35 +0300

    add a once initialising method to make http requests.

--- app/src/main/java/com/example/news/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/news/api/RetrofitInstance.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class RetrofitInstance {
     companion object{
@@ -19,6 +20,10 @@ class RetrofitInstance {
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
+        }
+
+        val apiInstance by lazy {
+            retrofit.create(NewsApi::class.java)
         }
     }
 }
