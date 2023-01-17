d6e747891388a9f804ba7db377e060f63850caa6
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-17 21:35:24 +0300

    create a method that converts a string to a source reference type.

--- app/src/main/java/com/example/news/converters/Converter.kt
+++ app/src/main/java/com/example/news/converters/Converter.kt
@@ -9,6 +9,10 @@ class Converter {
      fun fromSource(source: Source): String{ //converts the source class to a string to be stored in the db
          return source.name
      }
+    @TypeConverter
+    fun toSource(name:String):Source{ //converts the string stored in the database as source back to its original source
+        return Source(name, name)
+    }
 
 
 }
