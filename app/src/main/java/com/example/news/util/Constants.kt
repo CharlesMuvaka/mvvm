d588b8b961809ebb909f0f8df0810984cd02086e
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-17 08:14:28 +0300

    add a base url for the api.

--- app/src/main/java/com/example/news/util/Constants.kt
+++ app/src/main/java/com/example/news/util/Constants.kt
@@ -3,5 +3,6 @@ package com.example.news.util
 class Constants {
     companion object{
         const val API_KEY = "c6d3f53cb0794b8f9b70a7f35be378b6"
+        const val BASE_URL = "http://newsapi.org/"
     }
 }
