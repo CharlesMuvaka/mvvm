e02683eeb8a98d9ac8399e3a3e18149a441bed23
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-17 21:26:07 +0300

    create a method that retrives all articles from the database.

--- app/src/main/java/com/example/news/database/NewsDao.kt
+++ app/src/main/java/com/example/news/database/NewsDao.kt
@@ -1,12 +1,17 @@
 package com.example.news.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.news.models.ArticleX
 
 @Dao
 interface NewsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(article:ArticleX):Long //inserts or updates an entity in the db
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<ArticleX>>
 }
