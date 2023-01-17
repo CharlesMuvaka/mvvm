24d5808b84c239859b9bf77fd3f5cd097ec44ed7
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-17 07:52:33 +0300

    create a method to retrieve breaking news from the api.

---
+++ app/src/main/java/com/example/news/api/NewsApi.kt
@@ -0,0 +1,17 @@
+package com.example.news.api
+
+import com.example.news.util.Constants.Companion.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode:String = "us",
+        @Query("page")
+        pageNumber:Int = 1,
+        @Query("api_key")
+        apiKey:String = API_KEY
+    )
+}
