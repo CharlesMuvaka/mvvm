8ee1738283628dc02e4468db14e112c11f9da708
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-18 18:15:14 +0300

    define a classes that implement the Resource class

--- app/src/main/java/com/example/news/models/Resource.kt
+++ app/src/main/java/com/example/news/models/Resource.kt
@@ -1,5 +1,7 @@
 package com.example.news.models
 
 sealed class Resource<T> (val data: T? = null, val message: String? = null){
-
+    class Success<T>(private val newsData:T): Resource<T>(newsData)
+    class Failure<T>(private val newsData:T? = null, private val errorMessage:String): Resource<T>(newsData,errorMessage )
+    class Loading<T>: Resource<T>()
 }
