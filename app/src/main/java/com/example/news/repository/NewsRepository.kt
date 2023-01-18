14095b296435ac373112610255ccb40872ea0400
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-18 19:00:53 +0300

    fix: add a return type to the method.

--- app/src/main/java/com/example/news/repository/NewsRepository.kt
+++ app/src/main/java/com/example/news/repository/NewsRepository.kt
@@ -2,10 +2,12 @@ package com.example.news.repository
 
 import com.example.news.api.RetrofitInstance
 import com.example.news.database.NewsDatabase
+import com.example.news.models.Article
+import retrofit2.Response
 
 class NewsRepository(val db: NewsDatabase) {
 
-    suspend fun getBreakingNews(countryCode: String, pageNumber:Int){
-        RetrofitInstance.apiInstance.getBreakingNews(countryCode, pageNumber)
+    suspend fun getBreakingNews(countryCode: String, pageNumber:Int): Response<Article>{
+        return RetrofitInstance.apiInstance.getBreakingNews(countryCode, pageNumber)
     }
 }
