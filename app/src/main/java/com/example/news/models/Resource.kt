ee5bc14ad01732d30b4fcdc0ae77b4b1d36d8be5
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-18 19:02:06 +0300

    fix: reorder the member variables.

--- app/src/main/java/com/example/news/models/Resource.kt
+++ app/src/main/java/com/example/news/models/Resource.kt
@@ -2,6 +2,6 @@ package com.example.news.models
 
 sealed class Resource<T> (val data: T? = null, val message: String? = null){
     class Success<T>(private val newsData:T): Resource<T>(newsData)
-    class Failure<T>(private val newsData:T? = null, private val errorMessage:String): Resource<T>(newsData,errorMessage )
+    class Failure<T>( private val errorMessage:String, private val newsData:T? = null,): Resource<T>(newsData,errorMessage )
     class Loading<T>: Resource<T>()
 }
