6e1e1bc991924d98e1f3e2ed4ddce50095c04971
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-18 18:17:54 +0300

    create a method to get breaking news from the api.

--- app/src/main/java/com/example/news/repository/NewsRepository.kt
+++ app/src/main/java/com/example/news/repository/NewsRepository.kt
@@ -1,6 +1,11 @@
 package com.example.news.repository
 
+import com.example.news.api.RetrofitInstance
 import com.example.news.database.NewsDatabase
 
 class NewsRepository(val db: NewsDatabase) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNumber:Int){
+        RetrofitInstance.apiInstance.getBreakingNews(countryCode, pageNumber)
+    }
 }
