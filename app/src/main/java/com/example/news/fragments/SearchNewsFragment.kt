29cd4158029a5ba2b7921833e46f267f7a8a585e
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-18 04:01:28 +0300

    get a view model instance from the parent activity.

--- app/src/main/java/com/example/news/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/news/fragments/SearchNewsFragment.kt
@@ -1,7 +1,18 @@
 package com.example.news.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.example.news.MainActivity
 import com.example.news.R
 
 class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
+    lateinit var newsViewModel: ViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        newsViewModel = (activity as MainActivity).viewModel
+    }
 }
