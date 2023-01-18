18a72b8d77f73238756ffc6ed5d907602ceeb658
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-18 18:46:33 +0300

    create a mutable live data instance to post values to the observers

--- app/src/main/java/com/example/news/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/news/viewmodels/NewsViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.news.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.news.models.Article
+import com.example.news.models.Resource
 import com.example.news.repository.NewsRepository
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class NewsViewModel(val repository: NewsRepository): ViewModel() {
+class NewsViewModel(private val repository: NewsRepository): ViewModel() {
+    private val breakingNews: MutableLiveData<Resource<Article>> = MutableLiveData()
+    var pageNumber: Int = 1
+
+
 }
